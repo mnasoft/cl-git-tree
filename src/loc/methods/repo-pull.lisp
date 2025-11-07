ac8fb2f154ab1e709f097c81b4f277d98e3925ea
Mykola Matvyeyev <mnasoft@gmail.com>
2025-11-07 19:28:36 +0200

    Пт 07 ноя 2025 19:28:36 EET

---
+++ src/loc/methods/repo-pull.lisp
@@ -0,0 +1,25 @@
+(in-package :cl-git-tree/loc)
+
+(defmethod repo-pull ((provider <provider>) (ws <workspace>)
+                      &key (remote (<location>-id ws)) branch rebase ff-only &allow-other-keys)
+  "Выполнить git pull из указанного remote."
+  (let* ((root (git-root ws))
+         (args '()))
+    (when branch   (push  branch args))
+    (push remote args)
+    (when ff-only  (push  "--ff-only" args))
+    (when rebase   (push  "--rebase" args))
+    (push "pull" args)
+    (push "git"  args)
+    (setf args (nreverse args))
+    ;; запуск
+    (multiple-value-bind (stdout stderr code)
+        (apply #'cl-git-tree/shell-utils:shell-run root args)
+      (declare (ignore stderr))
+      (cond
+        ((zerop code)
+         (format t "📥 Репозиторий ~A успешно обновлён из ~A~%"
+                 (repo-name ws) remote))
+        (t
+         (format t "❌ Ошибка при pull из ~A: ~A~%" remote stdout))))
+    ws))
---
+++ src/loc/methods/repo-push.lisp
@@ -0,0 +1,25 @@
+(in-package :cl-git-tree/loc)
+
+(defmethod repo-push ((provider <provider>) (ws <workspace>)
+                      &key (remote (<location>-id provider)) branch force tags set-upstream
+                      &allow-other-keys)
+  "Выполнить git push на GitHub."
+  (let ((root (git-root ws))
+        (args '()))
+    (when branch       (push branch           args))
+    (when force        (push "--force"        args))
+    (when tags         (push "--tags"         args))
+    (when set-upstream (push "--set-upstream" args))
+    (push remote  args)
+    (push "push"  args)
+    (push "git"   args)
+    (setf args (nreverse args))
+    (multiple-value-bind (stdout stderr code)
+        (apply #'cl-git-tree/shell-utils:shell-run root args)
+      (cond
+        ((zerop code)
+         (format t "🚀 Репозиторий ~A успешно отправлен на ~A~%"
+                 (repo-name ws) remote))
+        (t
+         (format t "❌ Ошибка при push на ~A: ~A ~A~%" remote stdout stderr))))
+    ws))
